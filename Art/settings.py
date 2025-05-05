9fa95d0fcf13ee652c1062584727a7e4c2c74b55
Minh <minhnguyen47431@gmail.com>
2025-05-05 22:26:34 +0700

    fix url

--- Art/settings.py
+++ Art/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_REDIRECT_URL = 'login'
+LOGIN_URL = 'login'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
