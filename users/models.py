949417225d7aa1feb55acda5cd0dfa7cc709a2d1
Tran Thi Huyen <tranthihuyenaaaaa@gmail.com>
2025-05-05 20:26:06 +0700

    set dataset

--- users/models.py
+++ users/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from upload.models import Dataset
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     image = models.ImageField(default='users/donate.jpg', upload_to='users/')
+    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE, related_name='profile', blank=True, null=True)
     
     def __str__(self):
         return f'{self.user.username} Profile'
