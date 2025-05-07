fea481a6d63c1cee02f68c96582dabbd1c6081e0
Tran Thi Huyen <tranthihuyenaaaaa@gmail.com>
2025-05-07 09:16:55 +0700

    fix bug

--- users/admin.py
+++ users/admin.py
@@ -4,4 +4,4 @@ from .models import Profile
 # Register your models here.
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'dataset__name')
+    list_display = ('user', 'dataset__name', 'updated_at')
