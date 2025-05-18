f5ab80baaddbf67b8dff5cf4f432bf199918044c
xiao ming <minhnguyen47431@gmail.com>
2025-05-19 00:18:22 +0700

    fix

--- Art/settings.py
+++ Art/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-jmfpwfn2bz7!&-r_hv41n_i6t^7*p(5nt!r8kgtz*$u7oxyph7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['sunset-y1fu.onrender.com', 'localhost', '0.0.0.0']
 
--- predict/process.py
+++ predict/process.py
@@ -1,15 +1,40 @@
 import pandas as pd
-from models.utils import add_feature, transform_ctr_encoding, transform_min_max_scaler, transform_target_encoding, target_columns, pca, d1, d2
+from models.utils import add_feature, transform_ctr_encoding, transform_min_max_scaler, transform_target_encoding, target_columns, d1, d2
 import numpy as np
 import pickle
 from sklearn.decomposition import PCA
+import os
+import gdown
+
+def download_large_file(url, file_path):
+    print("Downloading large file...")
+    if "file/d/" in url:
+        file_id = url.split("file/d/")[1].split("/")[0]
+        url = f"https://drive.google.com/uc?id={file_id}"
+        print(url)
+    gdown.download(url, file_path, quiet=False)
+
 
 def load_toolkits():
-    with open(r'predict\toolkits\ctr_maps.pkl', 'rb') as f:
+    ctr_maps = r'predict\toolkits\ctr_maps.pkl'
+    encoder = r'predict\toolkits\encoder.pkl'
+    scaler = r'predict\toolkits\scaler.pkl'
+    
+    if not os.path.exists(ctr_maps):
+        download_large_file('https://drive.google.com/file/d/1-929DjQVxJ3K3c-bGbORvx-hr9SB6PAk/view?usp=drive_link', ctr_maps)
+        print('download ctr_maps.pkl successfully!')
+    if not os.path.exists(encoder):
+        download_large_file('https://drive.google.com/file/d/1-BYTPUt5r8Uxd9K5G1Y5sAJjONOpXpxj/view?usp=drive_link', encoder)
+        print('download encoder.pkl successfully!')
+    if not os.path.exists(scaler):
+        download_large_file('https://drive.google.com/file/d/1-JkmNR4R8eg0PKJkJkopC5dytQESyCry/view?usp=drive_link', scaler)
+        print('download scaler.pkl successfully!')
+        
+    with open(ctr_maps, 'rb') as f:
         ctr_maps = pickle.load(f)
-    with open(r'predict\toolkits\encoder.pkl', 'rb') as f:
+    with open(encoder, 'rb') as f:
         encoder = pickle.load(f)
-    with open(r'predict\toolkits\scaler.pkl', 'rb') as f:
+    with open(scaler, 'rb') as f:
         scaler = pickle.load(f)
         
     return ctr_maps, encoder, scaler
