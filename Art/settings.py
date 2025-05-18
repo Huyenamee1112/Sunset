1113afefdd2a5e7345d92b459ad474e1c6132108
xiao ming <minhnguyen47431@gmail.com>
2025-05-19 00:39:59 +0700

    fix

--- Art/settings.py
+++ Art/settings.py
@@ -141,4 +141,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 CSRF_TRUSTED_ORIGINS = ['https://sunset-y1fu.onrender.com']
